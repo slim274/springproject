8c26f87828e38ccb079ffe97b0ad614e102b7d6c
Slim <slimyoung274@gmail.com>
2025-05-17 21:57:37 +0100

    Add spring-petclinic project

---
+++ src/main/java/com/alpha/app/App.java
@@ -0,0 +1,10 @@
+package com.alpha.app;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Learning maven for jave development");
+    }
+}
